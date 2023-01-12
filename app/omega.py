08282048ed0bc533f73ee9e0f194276c0d85d58f
James Maxwell <jmaxwell@jlab.org>
2023-01-12 10:02:59 -0500

    Omega hex manipulation

--- app/omega.py
+++ app/omega.py
@@ -24,10 +24,18 @@ class Omega():
         #self.set_regex = re.compile('SP(\d) VALUE: (\d+.\d+)')  
         #self.ok_response_regex = re.compile(b'!a!o!\s\s')    
          
-    def set_setpoint(self):
+    def set_setpoint(self, sp):
         '''Change setpoint 1.'''  
+        
+        prefix = '*W01'    # write to sp1
+        eol = '\r'
+        value = '{:06X}'.format(int(spl)*10+1048576*2)    # hex magic
+        command = prefix+value+eol
+        print(command)
+        
         try: 
-            self.tn.write(bytes(f"SRDG? 0\n",'ascii'))     # 0 means it will return all channels
+            self.tn.write(bytes(command,'ascii'))     # 0 means it will return all channels            
+            self.tn.write(bytes('*R01','ascii'))    # readback setpoint
             data = self.tn.read_until(b'\n', timeout = 2).decode('ascii')   # read until carriage return
             m = self.pid_regex.search(data)
             values  = [float(x) for x in m.groups()]
