c95a3e9413d7ffa4b4fcbffcf3245be77e83c739
James Maxwell <jmaxwell@jlab.org>
2023-01-23 21:07:50 -0500

    Update to LS218 handler

--- app/temp_218.py
+++ app/temp_218.py
@@ -21,7 +21,7 @@ class ReadLS218():
         self.records = records
         self.settings = settings
         self.device_name = device_name
-        self.Is = self.records['Indicators']   # list of Flow Indicator names in channel order
+        self.Is = self.records['Indicators']   # list of Indicator names in channel order to associate PV with channel
         self.pvs = {}
         
         for pv_name in self.Is:      # Make AIn PVs for all FIs
@@ -30,25 +30,23 @@ class ReadLS218():
                 if not isinstance(value, dict):   # don't do the lists of states
                     setattr(self.pvs[pv_name], field, value)   # set the attributes of the PV        
                     
-      #  self.thread = FlowThread(self)
+        self.thread = LS218Thread(self)
         self.thread.setDaemon(True)
         self.thread.start()
         
 class LS218Thread(Thread):
 
     def __init__(self, parent):     
-        ''' Thread reads every iteration, gets settings from parent. fc_update is boolean telling thread to change set points also.
+        ''' Thread reads every iteration, gets settings from parent. c_update is boolean telling thread to change set points also.
         '''
         Thread.__init__(self)
         self.enable = parent.settings['enable'] 
         self.delay = parent.settings['delay']
         self.pvs = parent.pvs
         self.Is = parent.Is
-        self.Cs = parent.Cs
-        self.values = [0]*len(self.Is)   # list of zeroes to start return FIs
-        self.setpoints = [0]*len(self.Cs)   # list of zeroes to start readback FCs
-    #    if self.enable:                # if not enabled, don't connect
-  #          self.t = THCD(parent.settings['ip'], parent.settings['port'], parent.settings['timeout'])     # open telnet connection to flow controllers
+        self.values = [0]*len(self.Is)   # list of zeroes to start return Is
+        if self.enable:                # if not enabled, don't connect
+            self.t = LS218(parent.settings['ip'], parent.settings['port'], parent.settings['timeout'])     # open telnet connection to flow controllers
 
     def run(self):
         '''
@@ -56,23 +54,13 @@ class LS218Thread(Thread):
         '''
         while True:
             sleep(self.delay)
-            
-            for pv_name, bool in self.fc_update.items():
-                if bool:   # there has been a change in this FC, update it
-                    if self.enable:
-                        self.t.set_setpoint(self.Cs.index(pv_name)+1, self.pvs[pv_name].get())
-                    else:
-                        self.setpoints[self.Cs.index(pv_name)] = self.pvs[pv_name].get()   # for test, just echo back
-                    self.fc_update[pv_name] = False  
+
                 
             if self.enable:
-                self.setpoints = self.t.read_setpoints()   
                 self.values = self.t.read_all()
             else:
                 self.values = [random.random() for l in self.values]    # return random number when we are not enabled
             for i, pv_name in enumerate(self.Is):
-                self.pvs[pv_name].set(self.values[i])                
-            for i, pv_name in enumerate(self.Cs):
-                self.pvs[pv_name].set(self.setpoints[i])    
+                self.pvs[pv_name].set(self.values[i])
 
   
