702bd0067969a2515cf59ad09f9fc359c4f9b965
jdmax <jmaxwell@jlab.org>
2023-02-22 15:09:13 -0500

    Comment

--- adc/dat8148.py
+++ adc/dat8148.py
@@ -24,7 +24,7 @@ class DAT8148():
         '''Read all input coils.'''
         try:
             values = self.m.read_coils(496,16)  # read all 16 channels starting at 496. Order is 8-16 then 0-7.
-            return values[8:] + values[:8]
+            return values[8:] + values[:8]  # switch around order to get 0-15
 
         except Exception as e:
             print(f"Datexel 8148 read failed on {self.host}: {e}")
