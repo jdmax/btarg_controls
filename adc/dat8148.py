fc83cb585e06b7ae2d5c54b30a63759025bde0bc
James Maxwell <jmaxwell@jlab.org>
2023-02-22 13:24:10 -0500

    DAT8148 working with unit

--- adc/dat8148.py
+++ adc/dat8148.py
@@ -21,10 +21,10 @@ class DAT8148():
             print(f"Datexel 8148 connection failed on {self.host}: {e}")
 
     def read_all(self):
-        '''Read all channels.'''
+        '''Read all input coils.'''
         try:
-            values = self.m.read_coils(505,16)  # read all 16 channels starting at 40
-            return values
+            values = self.m.read_coils(496,16)  # read all 16 channels starting at 496. Order is 8-16 then 0-7.
+            return values[8:] + values[:8]
 
         except Exception as e:
             print(f"Datexel 8148 read failed on {self.host}: {e}")
