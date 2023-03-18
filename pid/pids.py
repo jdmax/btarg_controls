15b587bc04cc418c293e5dcd21840b4147483e8c
jmaxwell <jdmax@mit.edu>
2023-03-17 23:20:59 -0400

    Fixes to PID

--- pid/pids.py
+++ pid/pids.py
@@ -1,9 +1,11 @@
-from softioc.builder import aOut
+from softioc.builder import aOut, boolOut
+from epics import caget
 from simple_pid import PID
 from collections import ChainMap
 from threading import Thread
 from time import sleep
 import numpy as np
+import argparse
 
 
 class PIDSetup():
@@ -13,42 +15,43 @@ class PIDSetup():
         '''Takes pids, a dictionary from config file
         '''
         self.pids = {}
-        for pid_name, pdict in pids.items():
-            in_pv = parent.pvs['input']    # get PVs from parent using pv_names
-            out_pv = parent.pvs['output']  # this needs to be from config file now, not parent!
-            self.pids[pid_name] = PIDLoop(in_pv, out_pv, pid_name, pdict)    
+        self.pid_settings = load_settings()
+        for pid_name, pdict in self.pid_settings.items():
+            self.pids[pid_name] = PIDLoop(pid_name, pdict)
             self.pids[pid_name].start()    # start thread to monitor pid
             
-        self.pvs = ChainMap([p.pvs for p in self.pids.values()])   # combine all the PID PVS
+        #self.pvs = ChainMap([p.pvs for p in self.pids.values()])   # combine all the PID PVS
     
 class PIDLoop(Thread):
     '''Instantiates a PID loop and thread, makes PVs
     '''
     
-    def __init__(self, in_pv, out_pv, pid_name, pdict):    
-        
+    def __init__(self, pid_name, pdict):
+        Thread.__init__(self)
         self.pvs = {}   # dict of PVs for this PID
-        self.in_pv = in_pv
-        self.out_out = out_pv
         self.pid_name = pid_name
-        self.update = dict(zip(pdict[outs].keys(),[False]*len(pdict[outs].keys())))   # True to update pv with the key as name
+        self.in_pv = pdict['input']
+        self.out_pv = pdict['output']
+        self.update = dict(zip(pdict['outs'].keys(),[False]*len(pdict['outs'].keys())))   # True to update pv with the key as name
         
-        for out, value in pdict[outs].items():
+        for out, value in pdict['outs'].items():   # make and set pvs with values from settings
             pv_name = pid_name+'_'+out      # PV names start with PID name
-            if isinstance(value, bool):             # setup PVs as boolean or analog    
+            if isinstance(value, bool):             # setup PVs as boolean or analog
                 self.pvs[pv_name] = boolOut(pv_name, on_update_name = self.update_attr)
             else:
                 self.pvs[pv_name] = aOut(pv_name, on_update_name = self.update_attr)
             self.pvs[pv_name].set('value', value)
         
         self.pid = PID()    # set up simple_pid 
-        for out in pdict[outs].keys():   # set all parameters to pid object except the max and min change
+        for out in pdict['outs'].keys():   # set all parameters to pid object except the max and min change
             if not 'change' in out:
-                setattr(self.pid, key, self.pvs[pid_name+'_'+out].get()) 
-        self.pid.output_limits = (self.out_pv.DRVL.get(), self.out_pv.DRVH.get())  # set limits based on drive limits from output PV
+                setattr(self.pid, out, self.pvs[pid_name+'_'+out].get())
+        low = caget(self.out_pv+'DRVL')
+        high = caget(self.out_pv+'DRVH')
+        self.pid.output_limits = (low, high)  # set limits based on drive limits from output PV
         self.delay = self.pvs[pid_name+'_'+'sample_time'].get()
-        self.max_change = self.pvs[pid_name+'_'+'max_change'].get())
-        self.min_change = self.pvs[pid_name+'_'+'min_change'].get())
+        self.max_change = self.pvs[self.pid_name+'_'+'max_change'].get()
+        self.min_change = self.pvs[self.pid_name+'_'+'min_change'].get()
             
     def run(self):    
         print('Started thread for', self.pid_name)
@@ -59,17 +62,17 @@ class PIDLoop(Thread):
                 if bool:   # there has been a change in this out pv, update it in the pid
                     if 'auto_mode' in pv_name:
                         self.pid.set_auto_mode( 
-                            self.pvs[pid_name+'_auto_mode'].get(), 
+                            self.pvs[self.pid_name+'_auto_mode'].get(),
                             last_output = self.out_pv.get()
                         )   # if turning on, start at previous output value
                     if 'change' in pv_name:
-                        self.min_change = self.pvs[pid_name+'_min_change'].get())
-                        self.max_change = self.pvs[pid_name+'_max_change'].get())                        
+                        self.min_change = self.pvs[self.pid_name+'_min_change'].get()
+                        self.max_change = self.pvs[self.pid_name+'_max_change'].get()
                     if 'sample_time' in pv_name:
-                        self.delay = self.pvs[pid_name+'_'+pv_name].get()
-                        setattr(self.pid, pv_name, self.pvs[pid_name+'_'+pv_name].get())
+                        self.delay = self.pvs[self.pid_name+'_'+pv_name].get()
+                        setattr(self.pid, pv_name, self.pvs[self.pid_name+'_'+pv_name].get())
                     else:    
-                        setattr(self.pid, pv_name, self.pvs[pid_name+'_'+pv_name].get())
+                        setattr(self.pid, pv_name, self.pvs[self.pid_name+'_'+pv_name].get())
 
             last_output = self.pid._last_output
             input = self.in_pv.get()
@@ -91,3 +94,16 @@ class PIDLoop(Thread):
         '''
         pv_name = pv.replace(self.device_name+':'+self.pid_name, '')   # remove device name from PV to get bare out pv name
         self.update[pv_name] = True
+
+def load_settings():
+    '''Load pid settings and records from YAML settings files. Argument parser allows '-s' to give a different folder'''
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--Settings", help = "Settings files folder")
+    args = parser.parse_args()
+    folder = args.Settings if args.Settings else '..'   # default is directory above
+
+    with open(f'{folder}/pids.yaml') as f:  # Load settings from YAML files
+        pids = yaml.load(f, Loader=yaml.FullLoader)
+    print(f"Loaded device settings from {folder}/pids.yaml.")
+    return pids
