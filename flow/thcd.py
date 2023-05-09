26e7dc4eb5b9ddb68886caf638d011130fed800f
jdmax <jdmax@mit.edu>
2023-05-09 14:04:45 -0400

    Added serial comms to THCD for flow controller

--- flow/thcd.py
+++ flow/thcd.py
@@ -1,5 +1,6 @@
 import telnetlib
 import re
+import serial
 
 class THCD():
     '''Handle connection to Teledyne Hastings THCD-401 via Telnet.     
@@ -71,4 +72,89 @@ class THCD():
             self.tn.close()
         except Exception as e:
             print(e)
-         
+
+
+class THCDserial():
+    '''Handle connection to Teledyne Hastings THCD-401 via USB serial connection.
+    '''
+
+    def __init__(self):
+        '''Open connection to Flow Controller
+        Arguments:
+            host: IP address
+            port: Port of device
+            timeout: Telnet timeout in secs
+        '''
+        self.port = '/dev/ttyUSB0'
+
+        try:
+            self.s = serial.Serial(self.port, baudrate=57600, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, timeout=1)
+        except Exception as e:
+            print(f"THCD serial connection failed on {self.port}: {e}")
+
+        self.read_regex = re.compile(
+            'READ:(-*\d+.\d+|!RANGE!),(-*\d+.\d+|!RANGE!),(-*\d+.\d+|!RANGE!),(-*\d+.\d+)|!RANGE!,')
+        self.set_regex = re.compile('SP(\d) VALUE: (\d+.\d+)')
+        self.ok_response_regex = re.compile(b'!a!o!\s\s')
+
+    def read_all(self):
+        '''Read all channels. Returns list of 4 readings.'''
+        try:
+            self.s.flushInput()
+            self.s.flushOutput()
+
+            self.s.write(("ar"+"\n").encode())
+            self.s.readline().decode("utf-8")    # read back command
+            out = self.s.readline().decode("utf-8")  # read back result
+            self.s.readline().decode("utf-8")   # read back ok
+
+            m = self.read_regex.search(out)
+            values = [float(x) if not 'RANGE' in x else 999 for x in m.groups()]
+            return values
+
+        except Exception as e:
+            raise OSError('THCD read')
+            print(f"THCD read failed on {self.host}: {e}")
+
+    def set_setpoint(self, channel, value):
+        '''Set set points for given channel. Returns list of 4 setpoints after setting.'''
+        try:
+            self.s.flushInput()
+            self.s.flushOutput()
+
+            self.s.write((f"aspv {channel}, {value}\n").encode())
+            self.s.readline().decode("utf-8")    # read back command
+            self.s.readline().decode("utf-8")   # read back ok
+            return True
+
+        except Exception as e:
+            raise OSError('THCD write SP')
+            print(f"THCD write setpoint failed on {self.host}: {e}")
+
+    def read_setpoints(self):
+        '''Read set points for all channels. Returns list of 4 setpoints.'''
+        values = []
+        try:
+            self.s.flushInput()
+            self.s.flushOutput()
+
+            self.s.write((f"aspv?\n").encode())
+            self.s.readline().decode("utf-8")    # read back command
+            out = self.s.readline().decode("utf-8")  # read back result
+            self.s.readline().decode("utf-8")   # read back ok
+
+            ms = self.set_regex.findall(out)
+            for m in ms:
+                values.append(float(m[1]))
+            return values
+
+        except Exception as e:
+            raise OSError('THCD read SP')
+            print(f"THCD read setpoint failed on {self.host}: {e}")
+
+    def __del__(self):
+        try:
+            self.s.close()
+        except Exception as e:
+            print(e)
+
