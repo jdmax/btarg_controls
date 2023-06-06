563a80feaa5d5d5c1cc1b7f5ca9879e1cf7c832c
jdmax <jdmax@mit.edu>
2023-06-06 15:21:55 -0400

    Add motor interface

--- devices/zaber_motor.py
+++ devices/zaber_motor.py
@@ -134,12 +134,3 @@ class DeviceConnection():
     def away(self, axis):
         self.axes[axis].generic_command('tools gotolimit away pos 1 0')
         return self.get_pos(axis)
-
-# from zaber example:
-#axis1_coroutine = axis1.move_absolute_async(3, Units.LENGTH_CENTIMETRES)
-#axis2_coroutine = axis2.move_absolute_async(3, Units.LENGTH_CENTIMETRES)
-
-#move_coroutine = asyncio.gather(axis1_coroutine, axis2_coroutine)
-
-#loop = asyncio.get_event_loop()
-#loop.run_until_complete(move_coroutine)
